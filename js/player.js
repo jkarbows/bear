4d219cbe0da8bbfab12712186d9208ed73ef1784
jkarbows <jkarbows@gmail.com>
2015-11-02 18:13:31 -0500

    rewrote control scheme, rotation broken as fuck but code is starting to be refactored more nicely :)

--- js/player.js
+++ js/player.js
@@ -1,3 +1,5 @@
+var playerSpeed = 4; playerVelY = 0; playerVelX = 0; free = true;
+
 function playerMovement() {
 	if (Key.isDown(Key.A)){
 		if(free){
@@ -38,4 +40,32 @@ function playerMovement() {
 	
 	pulseBox.position.y += playerVelY;
 	pulseBox.position.x += playerVelX;
-}
+};
+
+
+//for some reason this broke when I added an init() function
+//and for some reason moving it here made it work again
+var torVelX = 1, torVelY = 1, torSpeed = 2;
+function torPhys(){
+	torK.position.x += torVelX * torSpeed;
+	torK.position.y += torVelY * torSpeed;
+	//shitty bounce logic
+	if (torK.position.y <= -200){
+		torVelY = -torVelY;
+	}
+	if (torK.position.y >= 200){
+		torVelY = -torVelY;
+	}
+	if (torK.position.x <= -400){
+		torVelX = -torVelX;
+	}
+	if (torK.position.x >= 400){
+		torVelX = -torVelX;
+	}
+	//speed limit
+	if (torVelY > torSpeed*2){
+		torVelY = torSpeed*2;
+	} else if(torVelY < -torSpeed*2){
+		torVelY = -torSpeed*2;
+	}
+};
